a55825187895cddfa6d431096b2b8492df9b05b5
evon <trananhtuan400@gmail.com>
2018-08-10 11:33:03 +0700

    add projects page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is listen on port ${port}`);
 });
